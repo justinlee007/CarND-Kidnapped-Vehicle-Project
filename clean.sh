317eb52f439d299573933c64e9075d1b6f2f19a6
Justin Lee <justin.lee007@gmail.com>
2017-06-03 12:43:05 -0700

    More formatting and clean up

--- clean.sh
+++ clean.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Script to clean the tree from all compiled files.
 # You can rebuild them afterwards using "build.sh".
-#
-# Written by Tiffany Huang, 12/14/2016
 #
 
 # Remove the dedicated output directories
--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Script to run particle filter!
-#
-# Written by Tiffany Huang, 12/14/2016
 #
 
 # Run particle filter
